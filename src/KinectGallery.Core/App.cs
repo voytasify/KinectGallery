a5b91a2577125f5028b665f9411c687194e3f4ae
voytasify <qdirqa@gmail.com>
2016-12-03 23:10:32 +0100

    Setup

---
+++ src/KinectGallery.Core/App.cs
@@ -0,0 +1,16 @@
+﻿using KinectGallery.Core.ViewModels;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+
+namespace KinectGallery.Core
+{
+	public class App : MvxApplication
+	{
+		public override void Initialize()
+		{
+			base.Initialize();
+
+			Mvx.RegisterSingleton<IMvxAppStart>(new MvxAppStart<PhotoGalleryViewModel>());
+		}
+	}
+}
---
+++ src/KinectGallery.Wpf/Setup.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Threading;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Wpf.Platform;
+using MvvmCross.Wpf.Views;
+
+namespace KinectGallery.Wpf
+{
+	public class Setup : MvxWpfSetup
+	{
+		public Setup(Dispatcher uiThreadDispatcher, IMvxWpfViewPresenter presenter) 
+			: base(uiThreadDispatcher, presenter)
+		{
+		}
+
+		protected override IMvxApplication CreateApp()
+			=> new Core.App();
+
+		protected override IMvxWpfViewsContainer CreateWpfViewsContainer()
+		{
+			var container = base.CreateWpfViewsContainer();
+//			container
+			return base.CreateWpfViewsContainer();
+		}
+	}
+}
